bc547cc7bae07897aa78bf1c46ac1fb5b123264b
somename <someemail>
2015-10-22 11:43:32 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -3,11 +3,14 @@ package org.unioulu.tol.sqat2015.planetExplorer.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsGridCreatedCorrectly() 
+	{
+		PlanetExplorer planetExplorer = new PlanetExplorer(3,3,"02/22");
+	
 	}
 }
